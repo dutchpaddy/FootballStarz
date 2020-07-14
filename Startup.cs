5f0353620071b6d4a2c649b568f162345e95beaf
Eric van Berkel <vanberkel.eric@gmail.com>
2020-07-14 23:14:03 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using FootballStarz.Data;
+using Microsoft.EntityFrameworkCore;
+using FootballStarz.Services;
+using Microsoft.AspNetCore.Identity;
+using FootballStarz.Models;
+
+namespace FootballStarz
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var _connection = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlServer(_connection));
+            services.AddTransient<IClubService, ClubService>();
+            services.AddTransient<IStadiumService, StadiumService>();
+            services.AddTransient<IPlayerService, PlayerService>();
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+            services.Configure<IdentityOptions>(options =>
+            {
+                //password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireUppercase = true;
+
+                //user settings
+                options.User.RequireUniqueEmail = true;
+
+                //lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+            });
+            services.ConfigureApplicationCookie(options => 
+            {
+                options.Cookie.HttpOnly = true;
+                options.Cookie.Expiration = TimeSpan.FromDays(5);
+                options.LoginPath = "/Account/Login";
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, AppDbContext context)
+        {
+            context.Database.Migrate();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                //app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/Home/Error");
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseStatusCodePages();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=School}/{action=All}/{id?}");
+            });
+        }
+    }
+}
